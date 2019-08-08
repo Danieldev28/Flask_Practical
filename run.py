529af501647511561334c1052813b0b87296c154
Ubuntu <ubuntu@ip-172-31-80-118.ec2.internal>
2019-08-08 13:23:14 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+    
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/about/what/')
+def about():
+    return render_template("about.html")
+
+if __name__ =='__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+    
+    
